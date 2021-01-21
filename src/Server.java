4a08af123c9cae1062f6888dce727b99b3df0ea5
lilf4p <lostoppa999@gmail.com>
2021-01-21 12:42:39 +0100

    Struttura del server con selector e connessione tcp

--- src/Server.java
+++ src/Server.java
@@ -1,14 +1,27 @@
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.net.ServerSocket;
+import java.nio.ByteBuffer;
+import java.nio.channels.SelectionKey;
+import java.nio.channels.Selector;
+import java.nio.channels.ServerSocketChannel;
+import java.nio.channels.SocketChannel;
+import java.nio.charset.StandardCharsets;
 import java.rmi.RemoteException;
 import java.rmi.server.RemoteServer;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Set;
 
 public class Server extends RemoteServer implements ServerInt {
 
+    final static int DEFAULT_PORT = 2000;
+    final static int MAX_SEG_SIZE = 512;
     private LoginInfoList listReg;
 
-    //metodo rmi
+    //metodo rmi x registrazione
     public String register(String nickUtente, String password) throws RemoteException {
         //if (nickUtente==null) return "Error - username must be insert!";
         //if (password==null) return "Error - password must be insert!";
@@ -35,8 +48,81 @@ public class Server extends RemoteServer implements ServerInt {
 
     public static void main(String[] args) {
 
+        //// TODO: 21/01/21 valuta come deve essere lanciato il server da terminale (serve porta?)
+        System.out.printf("Server starting ... listening on port "+DEFAULT_PORT);
 
+        //setto socket e apro il selector
+        ServerSocketChannel serverSocketChannel;
+        Selector selector;
+        try {
+            serverSocketChannel = ServerSocketChannel.open();
+            ServerSocket serverSocket = serverSocketChannel.socket();
+            serverSocket.bind(new InetSocketAddress(DEFAULT_PORT));
+            serverSocketChannel.configureBlocking(false);
+            selector = Selector.open();
+            serverSocketChannel.register(selector, SelectionKey.OP_ACCEPT);
+        } catch (IOException e) {
+            e.printStackTrace();
+            return;
+        }
 
+        while (true) {
+            try {
+                selector.select();
+            } catch (IOException e) {
+                e.printStackTrace();
+                break;
+            }
+            Set <SelectionKey> readyKeys = selector.selectedKeys();
+            Iterator <SelectionKey> iterator = readyKeys.iterator();
+            while (iterator.hasNext()) {
+                SelectionKey key = iterator.next();
+                iterator.remove();
+                //rimuove la chiave dal selected set , ma non dal registered set
+                try {
+                    if (key.isAcceptable()) {
+                        SocketChannel client = serverSocketChannel.accept();
+                        System.out.println("Connection successfully accepted from "+client);
+                        client.configureBlocking(false);
+                        SelectionKey keyClient = client.register(selector,SelectionKey.OP_READ);
+                        String s = "Benvenuto in Worth sviluppato da Leonardo Stoppani";
+                        byte [] bytes = s.getBytes();
+                        ByteBuffer output1 = ByteBuffer.allocate(bytes.length);
+                        output1.put(bytes);
+                        output1.flip();
+                        client.write(output1);
+                        System.out.println("SEND : Welcome msg\n");
+                    }else if (key.isReadable()) {
+                        SocketChannel client = (SocketChannel) key.channel();
+                        ByteBuffer input = ByteBuffer.allocate(MAX_SEG_SIZE);
+                        //leggo dal channel il comando
+                        if (client.read(input)==-1) {//client richiede chiusura --> chiudo connessione
+                            System.out.println("Closing connection with client...");
+                            key.cancel();
+                            key.channel().close();
+                            continue;
+                        }
+                        input.flip();//preparo buffer alla lettura
+                        String si = new String(input.array(), StandardCharsets.UTF_8);
+                        //debug
+                        System.out.println("FROM CLIENT : "+si);
+                        System.out.println(si.length());
+                        // TODO: 21/01/21 parsing dei comandi + implementazione comandi
+                        key.interestOps(SelectionKey.OP_WRITE);
+                    }else if (key.isWritable()) {
+                        key.interestOps(SelectionKey.OP_READ);
+                    }
+                } catch (IOException e) {
+                    key.cancel();
+                    try {
+                        key.channel().close();
+                    } catch (IOException ioException) {
+                        ioException.printStackTrace();
+                    }
+                    e.printStackTrace();
+                }
+            }
+        }
     }
 
 
